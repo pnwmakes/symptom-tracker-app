af9e36092a74bbf5ca19ee05daa130bbc0502eed
James Melzark <pnwmakes@gmail.com>
2025-06-03 16:42:03 -0700

    add console.log

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+window.onerror = function (message, source, lineno, colno, error) {
+    alert('Error: ' + message + '\nLine: ' + lineno);
+};
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
